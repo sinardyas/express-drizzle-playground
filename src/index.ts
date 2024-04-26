7f996d720312bf8861983dd7437e3cb4b0adbf66
sinardyas <sinardyas96@gmail.com>
2024-04-26 11:05:41 +0700

    chore: add cors config

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import express from "express";
 import type { Express } from "express";
 import helmet from "helmet";
+import cors from "cors";
 
 import apiV1Router from "./api/index.route";
 import { logger } from "./commons/utils/log";
@@ -17,6 +18,17 @@ app.use(express.json());
 
 // parse urlencoded request
 app.use(express.urlencoded({ extended: true }));
+
+/**
+ * CORS configuration
+ */
+app.use(
+    cors({
+      origin: process.env.CLIENT_URL || '*', // allow CORS
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true, // allow session cookie from browser to pass through
+    })
+  );
 
 app.use("/v1", apiV1Router);
 
